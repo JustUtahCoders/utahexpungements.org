b63797afa9b662e0d4e95e16ba89c1c53e50598f
lukeschunk <lukeschunk@gmail.com>
2018-08-06 20:22:14 -0600

    screening tool breadcrumbs WIP

--- src/app/screening-tool/screening-tool-crumb.component.js
+++ src/app/screening-tool/screening-tool-crumb.component.js
@@ -4,6 +4,8 @@ import Breadcrumb from '../breadcrumb.component.js'
 
 export default class ScreeningToolCrumb extends React.Component {
   render() {
+    console.log(111, this.props.match)
+
     const areYouEligibleRoute = this.props.match.url + '/are-you-eligible'
     return (
       <Route
@@ -12,7 +14,25 @@ export default class ScreeningToolCrumb extends React.Component {
           <Breadcrumb
             name="Step 1: Are you eligible?"
             url={areYouEligibleRoute}
-          />
+            childCrumbs={
+              <>
+                <Breadcrumb
+                  name="Do you have a Conviction?"
+                  url={areYouEligibleRoute}
+                />
+                <Route
+                  path={areYouEligibleRoute + '/pending'}
+                  render={() => (
+                    <Breadcrumb
+                      name="Do you have a case pending?"
+                      url={areYouEligibleRoute + '/pending'}
+                    />
+                  )}
+                />
+              </>
+            }
+          >
+          </Breadcrumb>
         )}
       />
     )
