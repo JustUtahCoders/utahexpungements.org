c47380f7c89e64dcf920a69ca63626b72f411316
Joel Denning <joeldenning@gmail.com>
2018-09-02 14:28:05 -0600

    Small fixes

--- src/app/forms/bank/fillable-form.component.js
+++ src/app/forms/bank/fillable-form.component.js
@@ -5,13 +5,10 @@ import {Link} from 'react-router-dom'
 
 export default class FillableForm extends React.Component {
   rootEl = React.createRef()
-  state = {
-    showDetails: false,
-  }
   render() {
     return (
       <Scoped css={css}>
-        <div className="fillable-form" onClick={this.openDetails} aria-label={__("click for form details")} tabIndex={0} role="button" onKeyPress={this.openDetails} ref={this.rootEl}>
+        <div className="fillable-form" ref={this.rootEl}>
           <div className="details">
             <div>
               <h4 className="name">
@@ -43,23 +40,6 @@ export default class FillableForm extends React.Component {
         </div>
       </Scoped>
     )
-  }
-  componentWillUnmount() {
-    if (this.state.showDetails) {
-      document.removeEventListener('click', this.closeDetails)
-    }
-  }
-  openDetails = (evt) => {
-    if (!evt.key || evt.key === 'Enter' || evt.key === 'Space') {
-      document.addEventListener('click', this.closeDetails)
-      this.setState({showDetails: true})
-    }
-  }
-  closeDetails = evt => {
-    if (!evt || !this.rootEl.current.contains(evt.target)) {
-      document.removeEventListener('click', this.closeDetails)
-      this.setState({showDetails: false})
-    }
   }
 }
 
