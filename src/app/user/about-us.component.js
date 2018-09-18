cdc93474b0e25ca82757c681c17eec5d9797460a
lukeschunk <lukeschunk@gmail.com>
2018-09-17 20:31:06 -0600

    added hoverin on the pics in about us section

--- src/app/user/about-us.component.js
+++ src/app/user/about-us.component.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Scoped } from 'kremling'
 
 class AboutUs extends React.Component {
   state = {
@@ -19,16 +20,59 @@ class AboutUs extends React.Component {
   render () {
     console.log('this is this.state', this.state)
     return (
-      <div><h1>Who We Are</h1>
-      
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
-          {this.state.people.map(person => <div><img src={person.avatar_url} style={{ width: '60%' }} /></div>)}
+      <Scoped css={css}>
+      <div className="container">
+        <h1>Who We Are</h1>
+
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+          {this.state.people.map(person => (
+
+            <div 
+              className="image-container"
+              style={{ position: 'relative', backgroundImage: `url(${person.avatar_url})`, cursor: 'pointer' }}
+              onClick={() => window.location = 'github.com/users/'}
+            >
+              <div className="name-card">{person.login}</div>
+            </div>
+            ))}
+          
 
         </div>
 
       </div>
+    </Scoped>
     )
   }
 }
 
+export const css = `
+  & .image-container {
+    margin-bottom: 10px;
+    width: 33%;
+    height: 250px;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: 290px;
+  }
+
+  & .name-card {
+    display: none;
+  }
+
+  & .image-container:hover > .name-card {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #A1674A;
+    color: white;
+    opacity: 0.85;
+  }
+
+
+  
+`
+
 export default AboutUs
+
