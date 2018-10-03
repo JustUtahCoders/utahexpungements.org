b115aad33ad778ea7fd3433f769f1169f8a36562
Joel Denning <joeldenning@gmail.com>
2018-10-03 13:57:01 -0600

    Migrating two more pdf components to PositionedString

--- src/app/forms/motion-to-waive-fees/motion-to-waive-fees.pdf.component.js
+++ src/app/forms/motion-to-waive-fees/motion-to-waive-fees.pdf.component.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import RenderPage from '../render-page.component.js'
-import {Scoped, a, m} from 'kremling'
+import PositionedString from '../pdf-rendering/positioned-string.component.js'
 
 export default class MotionToWaiveFees_Pdf extends React.Component {
   render() {
@@ -8,26 +8,13 @@ export default class MotionToWaiveFees_Pdf extends React.Component {
     return (
       <>
         <RenderPage url="/static/forms/motion-to-waive-fees/01_Motion_and_Affidavit_to_Waive_Fees-1.png">
-          <Scoped css={css}>
-            <div className="name">
-              {renderData('person.firstName')} {renderData('person.lastName')}
-            </div>
-          </Scoped>
+          <PositionedString debugKey="name" left="11%" top="14.04%">
+            {renderData('person.firstName')} {renderData('person.lastName')}
+          </PositionedString>
         </RenderPage>
         <RenderPage url="/static/forms/motion-to-waive-fees/01_Motion_and_Affidavit_to_Waive_Fees-2.png">
-          <Scoped css={css}>
-            <div>
-            </div>
-          </Scoped>
         </RenderPage>
       </>
     )
   }
 }
-
-const css = `
-  & .name {
-    left: 11%;
-    top: 13.94%;
-  }
-`
--- src/app/forms/petition-for-conviction/petition-for-conviction.pdf.component.js
+++ src/app/forms/petition-for-conviction/petition-for-conviction.pdf.component.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import moment from 'moment';
-
 import RenderPage from '../render-page.component.js'
-import {Scoped} from 'kremling'
+import PositionedString from '../pdf-rendering/positioned-string.component.js'
+import PositionedCheckmark from '../pdf-rendering/positioned-checkmark.component.js'
 
 export default class PetitionForConviction_Pdf extends React.Component {
   render() {
@@ -11,159 +11,42 @@ export default class PetitionForConviction_Pdf extends React.Component {
     return (
       <>
         <RenderPage url="/static/forms/petition-to-expunge-conviction/Petition_to_Expunge_Records_Criminal-conviction-1.png">
-          <Scoped css={css}>
-            <div className="name">
-              {`${renderData('person.firstName')} ${renderData('person.middleName')} ${renderData('person.lastName')}`}
-            </div>
-            {this.formItem('person.addressStreet')}
-            <div className="lineAddress">
-              {`${renderData('person.addressCity')} ${renderData('person.addressState')} ${renderData('person.addressZip')}`}
-            </div>
-            {this.formItem('person.dayPhone')}
-            {this.formItem('person.email')}
-            {this.checkMark('person.petitionerRepresentative', data.petitionerRepresentative === 'petitioner', 'petitioner')}
-            {this.checkMark('person.petitionerRepresentative', data.petitionerRepresentative === 'attorney', 'attorney')}
-            {this.formItem('person.barNumber')}
-            <div className="addressCourt">
-              {`${renderData('case.addressCourtStreet')}, ${renderData('case.addressCourtCity')}, ${renderData('case.addressCourtState')} ${renderData('case.addressCourtZip')}`}
-            </div>
-            <div className="petitionerName">
-              {`${renderData('person.firstName')} ${renderData('person.middleName')} ${renderData('person.lastName')}`}
-            </div>
-            {this.formItem('case.caseNumber')}
-            {this.formItem('case.judgeName')}
-            <div className="caseNumberConviction">
-              {/* This is wrapped in a div because it shares data , but needs own styles */}
-              {`${renderData('case.caseNumber')}`}
-            </div>
-            {this.formItem('case.publicInterest')}
-          </Scoped>
+          <PositionedString debugKey="name" left={farLeft} top="14.06%">
+            {`${renderData('person.firstName')} ${renderData('person.middleName')} ${renderData('person.lastName')}`}
+          </PositionedString>
+          <PositionedString dataKey="person.addressStreet" left={farLeft} top="17.67%" />
+          <PositionedString debugKey="lineAddress" left={farLeft} top="20.90%">
+            {`${renderData('person.addressCity')} ${renderData('person.addressState')} ${renderData('person.addressZip')}`}
+          </PositionedString>
+          <PositionedString dataKey="person.dayPhone" left={farLeft} top="24.75%" />
+          <PositionedString dataKey="person.email" left={farLeft} top="28.3%" />
+          <PositionedCheckmark debugKey="petitionerRepresentingThemself" left={petRepLeft} top="31.75%" shouldShow={data.person.petitionerRepresentative === 'petitioner'} />
+          <PositionedCheckmark debugKey="petitionerHasAttorney" left={petRepLeft} top="33.25%" shouldShow={data.person.petitionerRepresentative === 'attorney'} />
+          <PositionedString dataKey="person.barNumber" left={barNumberLeft} top="33.15" />
+          <PositionedString debugKey="addressCourt" left="27.8%" top="43.6%">
+            {`${renderData('case.addressCourtStreet')}, ${renderData('case.addressCourtCity')}, ${renderData('case.addressCourtState')} ${renderData('case.addressCourtZip')}`}
+          </PositionedString>
+          <PositionedString debugKey="petitionerName" left="11.9%" top="53%">
+            {`${renderData('person.firstName')} ${renderData('person.middleName')} ${renderData('person.lastName')}`}
+          </PositionedString>
+          <PositionedString dataKey="case.caseNumber" left="54.1%" top="53%" />
+          <PositionedString dataKey="case.judgeName" left="54.1%" top="58%" />
+          <PositionedString dataKey="case.caseNumber" debugKey="caseNumberConviction" left="17.4%" top="65.8%" />
+          <PositionedString dataKey="case.publicInterest" left="17.4%" top="79%" style={{width: '74%', height: '15%', lineHeight: '30px', overflowY: 'hidden'}} />
         </RenderPage>
         <RenderPage url="/static/forms/petition-to-expunge-conviction/Petition_to_Expunge_Records_Criminal-conviction-2.png">
-          <Scoped css={css}>
-
-            <div className="todaysDate">
-              {moment().format('L')}
-            </div>
-            <div className="printedName">
-              {`${renderData('person.firstName')} ${renderData('person.middleName')} ${renderData('person.lastName')}`}
-            </div>
-          </Scoped>
+          <PositionedString debugKey="todaysDate" left="54.4%" top="39.7%">
+            {moment().format('L')}
+          </PositionedString>
+          <PositionedString debugKey="printedName" left="54.4%" top="39.7%">
+            {`${renderData('person.firstName')} ${renderData('person.middleName')} ${renderData('person.lastName')}`}
+          </PositionedString>
         </RenderPage>
       </>
     )
-  }
-
-  formItem = dataKey => {
-    const index = dataKey.indexOf('.');    
-    const trimmedName = dataKey.substring(index + 1);
-    return (
-      <div className={trimmedName}>
-        {this.props.renderData(dataKey)}
-      </div>
-    )
-  }
-
-  checkMark = (dataKey, isChecked, className=dataKey) => {
-    if (isChecked) {
-      return (
-        <div className={className}>
-          {'\u2714'}
-        </div>
-      ) 
-    } else {
-      return null;
-    }
   }
 }
 
 const farLeft = `11.28%`
 const petRepLeft = `20.82%`
 const barNumberLeft = `62.2%`
-
-const css = `
-  & .name {
-    left: ${farLeft};
-    top: 14.06%;
-  }
-
-  & .addressStreet {
-    left: ${farLeft};
-    top: 17.67%;
-  }
-
-  & .lineAddress {
-    left: ${farLeft};
-    top: 20.90%;
-  }
-
-  & .dayPhone {
-    left: ${farLeft};
-    top: 24.75%;
-  }
-
-  & .email {
-    left: ${farLeft};
-    top: 28.3%;
-  }
-
-  & .petitioner {
-    left: ${petRepLeft};
-    top: 31.75%;
-  }
-
-  & .attorney {
-    left: ${petRepLeft};
-    top: 33.25%;
-  }
-
-  & .barNumber {
-    left: ${barNumberLeft};
-    top: 33.15%;
-  }
-
-  & .addressCourt {
-    left: 27.8%;
-    top: 43.6%;
-  }
-
-  & .petitionerName {
-    left: 11.9%;
-    top: 53%;
-  }
-
-  & .caseNumber {
-    left: 54.1%;
-    top: 53%;
-  }
-
-  & .judgeName {
-    left: 54.1%;
-    top: 58%;
-  }
-
-  & .caseNumberConviction {
-    left: 17.4%;
-    top: 65.8%;
-  }
-
-  & .publicInterest {
-    left: 17.4%;
-    top: 79%;
-    width: 74%;
-    height: 15%;
-    line-height: 30px;
-    overflow-y: hidden;
-  }
-
-  & .printedName {
-    left: 54.4%;
-    top: 39.7%;
-  }
-
-  & .todaysDate {
-    left: 10.9%;
-    top: 36.7%;
-  }
-
-`
