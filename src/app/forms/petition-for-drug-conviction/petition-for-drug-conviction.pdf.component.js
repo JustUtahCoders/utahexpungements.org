3b27b152d9421abc607a5c695dba4f4b878be539
Luke Schunk <lukeschunk@gmail.com>
2019-08-27 21:21:04 -0600

    Luke/drug form continued (#40)
    * continues work on drug form
    
    * adds more fields and renders to pdf for drug petition conviction form
    
    * prettier
    
    * prettier with proper config
    
    * removes logs
    
    * addresses pr feedback

--- src/app/forms/petition-for-drug-conviction/petition-for-drug-conviction.pdf.component.js
+++ src/app/forms/petition-for-drug-conviction/petition-for-drug-conviction.pdf.component.js
@@ -1,6 +1,7 @@
 import React from "react";
 import RenderPage from "../render-page.component.js";
 import PositionedString from "../pdf-rendering/positioned-string.component.js";
+import PositionedCheckmark from "../pdf-rendering/positioned-checkmark.component.js";
 
 export default class PetitionForDrugConviction_Pdf extends React.Component {
   render() {
@@ -8,7 +9,7 @@ export default class PetitionForDrugConviction_Pdf extends React.Component {
     return (
       <>
         <RenderPage url="/static/forms/petition-for-drug-conviction/01_Petition_to_Expunge_Records_Criminal-drug_possession-1.png">
-          <PositionedString debugKey="name" left="11%" top="13.94%">
+          <PositionedString left="11%" top="13.94%">
             {renderData("person.firstName")} {renderData("person.lastName")}
           </PositionedString>
           <PositionedString
@@ -16,7 +17,7 @@ export default class PetitionForDrugConviction_Pdf extends React.Component {
             left="11%"
             top="17.5%"
           />
-          <PositionedString debugKey="city-state-zip" left="11%" top="20.91%">
+          <PositionedString left="11%" top="20.91%">
             {renderData("person.addressCity")}
             {data.person.addressCity ? ", " : ""}{" "}
             {renderData("person.addressState")}{" "}
@@ -28,8 +29,77 @@ export default class PetitionForDrugConviction_Pdf extends React.Component {
             top="24.71%"
           />
           <PositionedString dataKey="person.email" left="11%" top="27.91%" />
+
+          <PositionedCheckmark
+            left="21%"
+            top="31.2%"
+            shouldShow={data.person.petitionerRepresentative === "petitioner"}
+          />
+          <PositionedCheckmark
+            left="21%"
+            top="33%"
+            shouldShow={data.person.petitionerRepresentative === "attorney"}
+          />
+
+          <PositionedCheckmark
+            left="37.5%"
+            top="36.5%"
+            shouldShow={data.case.courtType === "district"}
+          />
+          <PositionedCheckmark
+            left="48%"
+            top="38%"
+            shouldShow={data.case.courtType === "justice"}
+          />
+
+          <PositionedString
+            dataKey="case.judicialDistrict"
+            left="27%"
+            top="40%"
+          />
+          <PositionedString dataKey="case.county" left="52%" top="40%" />
+          <PositionedString
+            dataKey="case.courtAddress"
+            left="27%"
+            top="42.5%"
+          />
+          <PositionedString dataKey="case.petitioner" left="11%" top="52.5%" />
+          <PositionedString dataKey="case.number" left="65%" top="52.5%" />
+          <PositionedString dataKey="case.number" left="25%" top="65.5%" />
+          <PositionedString dataKey="case.judgeName" left="65%" top="57.5%" />
+          {data.drugConviction && (
+            <>
+              <PositionedCheckmark
+                left="24%"
+                top="81.2%"
+                shouldShow={data.drugConviction.pryingQuestion === "no"}
+              />
+              <PositionedCheckmark
+                left="24%"
+                top="84%"
+                shouldShow={data.drugConviction.pryingQuestion === "yes"}
+              />
+            </>
+          )}
         </RenderPage>
-        <RenderPage url="/static/forms/petition-for-drug-conviction/01_Petition_to_Expunge_Records_Criminal-drug_possession-2.png"></RenderPage>
+        <RenderPage url="/static/forms/petition-for-drug-conviction/01_Petition_to_Expunge_Records_Criminal-drug_possession-2.png">
+          <PositionedString
+            dataKey="drugConviction.pryingQuestionAnswer"
+            left="17.4%"
+            top="10%"
+          />
+          <PositionedString
+            dataKey="case.publicInterest"
+            left="17.4%"
+            top="29%"
+            style={{
+              width: "74%",
+              height: "15%",
+              lineHeight: "30px",
+              overflowY: "hidden"
+            }}
+          />
+        </RenderPage>
       </>
     );
   }
--- src/app/forms/petition-for-drug-conviction/petition-for-drug-conviction.web.component.js
+++ src/app/forms/petition-for-drug-conviction/petition-for-drug-conviction.web.component.js
@@ -1,8 +1,35 @@
 import React from "react";
 import { Scoped } from "kremling";
 import TextInput from "../inputs/text-input.component.js";
+import Select from "../inputs/select.component.js";
+import TextArea from "../inputs/text-area.component.js";
 import FormThatPrints from "../inputs/form-that-prints.component.js";
 import Section from "../inputs/section.component.js";
+
+const petitionerRepresentativeOptions = [
+  { label: "Yes. I am the petitioner.", value: "petitioner" },
+  {
+    label: "No. I am an attorney representing the petitioner.",
+    value: "attorney"
+  }
+];
+
+const courtOptions = [
+  { label: "District", value: "district" },
+  { label: "Justice Court of Utah", value: "justice" }
+];
+
+const pryingQuestionOptions = [
+  {
+    label: "I have not been diagnosed as having a substance abuse addiction.",
+    value: "no"
+  },
+  {
+    label:
+      "I have been diagnosed as having a substance abuse addiction and I am managing my addiction by:",
+    value: "yes"
+  }
+];
 
 export default class PetitionForDrugConviction_Web extends React.Component {
   render() {
@@ -17,9 +44,48 @@ export default class PetitionForDrugConviction_Web extends React.Component {
             <TextInput dataKey="person.addressCity" label="City" />
             <TextInput dataKey="person.addressState" label="State" />
             <TextInput dataKey="person.addressZip" label="Zip" />
-            <TextInput dataKey="person.addressZip" label="Zip" />
 
             <TextInput dataKey="person.homePhone" label="Home Phone Number" />
+            <TextInput dataKey="person.email" label="Email" />
+            <Select
+              dataKey="person.petitionerRepresentative"
+              label={__("are you filling this out for yourself")}
+              options={petitionerRepresentativeOptions}
+            />
+
+            <Select
+              dataKey="case.courtType"
+              label="In which court was the case"
+              options={courtOptions}
+            />
+            <TextInput
+              dataKey="case.judicialDistrict"
+              label="Judicial District"
+            />
+            <TextInput dataKey="case.county" label="County" {...this.props} />
+            <TextInput dataKey="case.courtAddress" label="Court Address" />
+            <TextInput dataKey="case.petitioner" label="Petitioner Name" />
+            <TextInput dataKey="case.number" label="Case Number" />
+            <TextInput dataKey="case.judgeName" label="Judge Name" />
+            <Select
+              dataKey="drugConviction.pryingQuestion"
+              label="Choose One"
+              options={pryingQuestionOptions}
+            />
+            {this.props.data.drugConviction &&
+              this.props.data.drugConviction.pryingQuestion === "yes" && (
+                <TextInput
+                  dataKey="drugConviction.pryingQuestionAnswer"
+                  label="I am managing my addiction by:"
+                />
+              )}
+
+            <TextArea
+              dataKey="case.publicInterest"
+              label={__(
+                "explain why expunging this crime is not contrary to the public's interest"
+              )}
+            />
           </Section>
         </FormThatPrints>
       </Scoped>
