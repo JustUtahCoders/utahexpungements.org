23d25997ba77244bd86a36e3f535c6084e8fc4a5
Joel Denning <joeldenning@gmail.com>
2019-08-27 21:50:58 -0600

    Implementing majority of Acceptance of Service.

--- src/app/forms/acceptance-of-service/acceptance-of-service.pdf.component.js
+++ src/app/forms/acceptance-of-service/acceptance-of-service.pdf.component.js
@@ -2,15 +2,45 @@ import React from "react";
 import RenderPage from "../render-page.component";
 import PositionedString from "../pdf-rendering/positioned-string.component";
 
-export default function AcceptanceOfService_Pdf({ renderData }) {
+export default function AcceptanceOfService_Pdf({ renderData, data }) {
   return (
     <>
       <RenderPage url="/static/forms/acceptance-of-service/Acceptance_of_service-1.png">
         <PositionedString debugKey="person.fullName" left="11.5%" top="13.8%">
           <div>
-            {renderData("person.firstName")} {renderData("person.lastName")}
+            {renderData("person.firstName")} {renderData("person.middleName")}{" "}
+            {renderData("person.lastName")}
           </div>
         </PositionedString>
+        <PositionedString
+          dataKey="person.addressStreet"
+          left="11.65%"
+          top="17.49%"
+        />
+        <PositionedString
+          debugKey="petitionerAddress"
+          left="11.65%"
+          top="21.09%"
+        >
+          {data.person.addressCity && `${renderData("person.addressCity")}, `}
+          {renderData("person.addressState")}
+          {`\u0020`}
+          {renderData("person.addressZip")}
+        </PositionedString>
+        <PositionedString
+          dataKey="person.homePhone"
+          left="11.65%"
+          top="24.5%"
+        />
+        <PositionedString dataKey="person.email" left="11.65%" top="28.1%" />
+        <PositionedString debugKey="petitioner" left="11.85%" top="49.7%">
+          <div>
+            {renderData("person.firstName")} {renderData("person.middleName")}{" "}
+            {renderData("person.lastName")}
+          </div>
+        </PositionedString>
+        <PositionedString dataKey="case.caseNumber" left="54.05%" top="49.7%" />
+        <PositionedString dataKey="case.judgeName" left="54.05%" top="54.4%" />
       </RenderPage>
     </>
   );
--- src/app/forms/acceptance-of-service/acceptance-of-service.web.component.js
+++ src/app/forms/acceptance-of-service/acceptance-of-service.web.component.js
@@ -8,7 +8,18 @@ export default function AcceptanceOfService_Web(props) {
     <FormThatPrints>
       <Section name="1. Personal Information">
         <TextInput dataKey="person.firstName" label="First Name" />
+        <TextInput dataKey="person.middleName" label={__("middle name")} />
         <TextInput dataKey="person.lastName" label="Last Name" />
+        <TextInput dataKey="person.addressStreet" label={__("street")} />
+        <TextInput dataKey="person.addressCity" label={__("city")} />
+        <TextInput dataKey="person.addressState" label={__("state")} />
+        <TextInput dataKey="person.addressZip" label={__("zip")} />
+        <TextInput dataKey="person.homePhone" label={__("home phone")} />
+        <TextInput dataKey="person.email" label={__("email address")} />
+      </Section>
+      <Section name="2. Case Information">
+        <TextInput dataKey="case.caseNumber" label={__("case number")} />
+        <TextInput dataKey="case.judgeName" label={__("judge full name")} />
       </Section>
     </FormThatPrints>
   );
