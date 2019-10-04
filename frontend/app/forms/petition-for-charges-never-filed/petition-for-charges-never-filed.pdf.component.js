c1235fce80260e9531c596a46818f279c9dedc80
sungjin mo <Sungjin@sungjins-MacBook.local>
2019-10-03 21:14:55 -0600

    most of page 1 done

--- frontend/app/forms/petition-for-charges-never-filed/petition-for-charges-never-filed.pdf.component.js
+++ frontend/app/forms/petition-for-charges-never-filed/petition-for-charges-never-filed.pdf.component.js
@@ -1,5 +1,98 @@
 import React from "react";
+import RenderPage from "../render-page.component.js";
+import PositionedString from "../pdf-rendering/positioned-string.component.js";
+import PositionedCheckmark from "../pdf-rendering/positioned-checkmark.component.js";
+import {
+  getJudicialDistrict,
+  getCounty
+} from "../form-common-options/form-common-options";
 
-export default function PetitonForChargesNeverFiled_Pdf(props) {
-  return <div>Pdf Component</div>;
+export default function PetitonForChargesNeverFiled_Pdf({ data, renderData }) {
+  return (
+    <>
+      <RenderPage url="/static/forms/petition-for-charges-never-filed/01_Petition_to_Expunge_Records_Criminal-charges_never_filed-1.png">
+        <PositionedString left="11%" top="13.94%" debugKey="person.name">
+          {renderData("person.firstName")} {renderData("person.middleName")}{" "}
+          {renderData("person.lastName")}
+        </PositionedString>
+        <PositionedString
+          dataKey="person.addressStreet"
+          left="11%"
+          top="17.5%"
+        />
+        <PositionedString left="11%" top="20.91%" debugKey="person.address">
+          {renderData("person.addressCity")}
+          {data.person.addressCity ? ", " : ""}{" "}
+          {renderData("person.addressState")} {renderData("person.addressZip")}
+        </PositionedString>
+        <PositionedString dataKey="person.homePhone" left="11%" top="24.6%" />
+        <PositionedString dataKey="person.email" left="11%" top="27.91%" />
+        <PositionedCheckmark
+          dataKey="person.petitionerRepresentative"
+          left="21%"
+          top="31.2%"
+          shouldShow={data.person.petitionerRepresentative === "petitioner"}
+        />
+        <PositionedCheckmark
+          dataKey="person.petitionerRepresentative"
+          left="21%"
+          top="33%"
+          shouldShow={data.person.petitionerRepresentative === "attorney"}
+        />
+        <PositionedString
+          dataKey="person.petitionerBarNumber"
+          left="62%"
+          top="32.7%"
+        />
+
+        <PositionedCheckmark
+          dataKey="case.courtType"
+          left="37.5%"
+          top="36.5%"
+          shouldShow={data.case.courtType === "District"}
+        />
+        <PositionedCheckmark
+          dataKey="case.courtType"
+          left="48%"
+          top="38%"
+          shouldShow={data.case.courtType === "Justice"}
+        />
+
+        <PositionedString dataKey="case.judicialDistrict" left="27%" top="40%">
+          {data.case.courtAddress &&
+            getJudicialDistrict(data.case.courtAddress, data.case.courtType)}
+        </PositionedString>
+        <PositionedString dataKey="case.county" left="52%" top="40%">
+          {data.case.courtAddress &&
+            getCounty(data.case.courtAddress, data.case.courtType)}
+        </PositionedString>
+        <PositionedString dataKey="case.courtAddress" left="27%" top="42.5%" />
+        <PositionedString debugKey="petitionerName" left="11.9%" top="52.7%">
+          {renderData("person.firstName")} {renderData("person.middleName")}{" "}
+          {renderData("person.lastName")}
+        </PositionedString>
+        <PositionedString dataKey="case.number" left="65%" top="52.5%" />
+        <PositionedString dataKey="case.number" left="25%" top="65.5%" />
+        <PositionedString dataKey="case.judgeName" left="65%" top="57.5%" />
+        <PositionedString
+          dataKey="case.arrestedDate"
+          left="47.4%"
+          top="63.25%"
+        />
+      </RenderPage>
+      <RenderPage url="/static/forms/petition-for-charges-never-filed/01_Petition_to_Expunge_Records_Criminal-charges_never_filed-2.png">
+        <PositionedString
+          dataKey="case.publicInterest"
+          left="17.8%"
+          top="22%"
+          style={{
+            width: "74%",
+            height: "15%",
+            lineHeight: "30px",
+            overflowY: "hidden"
+          }}
+        />
+      </RenderPage>
+    </>
+  );
 }
--- frontend/app/forms/petition-for-charges-never-filed/petition-for-charges-never-filed.web.component.js
+++ frontend/app/forms/petition-for-charges-never-filed/petition-for-charges-never-filed.web.component.js
@@ -1,5 +1,83 @@
 import React from "react";
+import FormThatPrints from "../inputs/form-that-prints.component.js";
+import TextInput from "../inputs/text-input.component.js";
+import TextArea from "../inputs/text-area.component.js";
+import Section from "../inputs/section.component.js";
+import Select from "../inputs/select.component.js";
+import GroupSelect from "../inputs/group-select.component.js";
+import Radio from "../inputs/radio.component.js";
+import {
+  petitionerRepresentativeOptions,
+  courtTypeOptions,
+  JusticeCourtList,
+  DistrictCourtList
+} from "../form-common-options/form-common-options";
 
-export default function PetitonForChargesNeverFiled_Web(props) {
-  return <div>Web Component</div>;
+export default function PetitonForChargesNeverFiled_Web({ data }) {
+  return (
+    <FormThatPrints>
+      <Section name="1. Personal Information">
+        <TextInput dataKey="person.lastName" label={__("last name")} />
+
+        <TextInput dataKey="person.firstName" label={__("first name")} />
+
+        <TextInput dataKey="person.middleName" label={__("middle name")} />
+
+        <TextInput dataKey="person.addressStreet" label={__("street")} />
+
+        <TextInput dataKey="person.addressCity" label={__("city")} />
+
+        <TextInput dataKey="person.addressState" label={__("state")} />
+
+        <TextInput dataKey="person.addressZip" label={__("zip")} />
+
+        <TextInput dataKey="person.homePhone" label={__("phone")} />
+
+        <TextInput dataKey="person.email" label={__("email address")} />
+      </Section>
+      <Section name="2. Case Information">
+        <Select
+          dataKey="person.petitionerRepresentative"
+          label={__("are you filling this out for yourself")}
+          options={petitionerRepresentativeOptions}
+        />
+
+        {data.person.petitionerRepresentative === "attorney" && (
+          <TextInput
+            dataKey="person.petitionerBarNumber"
+            label="What is your Utah Bar number?"
+          />
+        )}
+
+        <Radio
+          label="Court Type"
+          dataKey="case.courtType"
+          options={courtTypeOptions}
+        />
+        {data.case.courtType === "District" && (
+          <GroupSelect
+            dataKey="case.courtAddress"
+            label="District Courts"
+            groupOptions={DistrictCourtList}
+          />
+        )}
+        {data.case.courtType === "Justice" && (
+          <GroupSelect
+            dataKey="case.courtAddress"
+            label="Justice Courts"
+            groupOptions={JusticeCourtList}
+          />
+        )}
+
+        <TextInput dataKey="case.caseNumber" label={__("case number")} />
+
+        <TextInput dataKey="case.judgeName" label={__("judge full name")} />
+        <TextInput dataKey="case.arrestedDate" label={__("arrested date")} />
+        <TextArea
+          dataKey="case.publicInterest"
+          label="explain why expunging this crime is not contrary to the public's interest"
+        />
+      </Section>
+    </FormThatPrints>
+  );
 }
