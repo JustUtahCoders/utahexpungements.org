3923dcce47d53f0577b0487ed7278fbd693dc28e
Guillermo Villalta <mexcelus@gmail.com>
2019-10-30 12:12:40 -0600

    added more text input + position strings

--- frontend/app/forms/order-on-motion-for-reduction-of-conviction/order-on-motion-for-reduction-of-conviction.pdf.component.js
+++ frontend/app/forms/order-on-motion-for-reduction-of-conviction/order-on-motion-for-reduction-of-conviction.pdf.component.js
@@ -1,5 +1,85 @@
 import React from "react";
+import RenderPage from "../render-page.component";
+import PositionedString from "../pdf-rendering/positioned-string.component";
+import PositionedCheckmark from "../pdf-rendering/positioned-checkmark.component.js";
+import {
+  getJudicialDistrict,
+  getCounty
+} from "../form-common-options/form-common-options";
 
-export default function OrderOnMotionForReductionOfConvinction_Pdf(props) {
-  return <div>Order on Motion for Reduction of Conviction PDF</div>;
+export default function OrderOnMotionForReductionOfConvinction_Pdf({
+  renderData,
+  data
+}) {
+  return (
+    <>
+      <RenderPage url="/static/forms/order-on-motion-for-reduction-of-conviction/05_Order_on_Motion_for_Reduction_of_Conviction-1.png">
+        <PositionedString
+          debugKey="person.fullName"
+          left={personalInfo}
+          top="13.8%"
+        >
+          <div>
+            {renderData("person.firstName")} {renderData("person.middleName")}{" "}
+            {renderData("person.lastName")}
+          </div>
+        </PositionedString>
+        <PositionedString
+          dataKey="person.addressStreet"
+          left={personalInfo}
+          top="17.49%"
+        />
+        <PositionedString
+          debugKey="petitionerAddress"
+          left={personalInfo}
+          top="21.09%"
+        >
+          {data.person.addressCity && `${renderData("person.addressCity")}, `}
+          {renderData("person.addressState")}
+          {`\u0020`}
+          {renderData("person.addressZip")}
+        </PositionedString>
+        <PositionedString
+          dataKey="person.homePhone"
+          left={personalInfo}
+          top="24.5%"
+        />
+        <PositionedString
+          dataKey="person.email"
+          left={personalInfo}
+          top="28.1%"
+        />
+        <PositionedString debugKey="petitioner" left={personalInfo} top="50.1%">
+          <div>
+            {renderData("person.firstName")} {renderData("person.middleName")}{" "}
+            {renderData("person.lastName")}
+          </div>
+        </PositionedString>
+        <PositionedString dataKey="case.caseNumber" left="54.05%" top="50.1%" />
+        <PositionedString dataKey="case.judgeName" left="54.05%" top="54.7%" />
+        <PositionedString
+          dataKey="case.courtAddress"
+          left="26.2%"
+          top="37.7%"
+        />
+        <PositionedCheckmark
+          dataKey="case.courtType"
+          left="36.2%"
+          top={courtTypeTop}
+          shouldShow={data.case.courtType === "District"}
+        />
+        <PositionedCheckmark
+          dataKey="case.courtType"
+          left="47%"
+          top={courtTypeTop}
+          shouldShow={data.case.courtType === "Justice"}
+        />
+      </RenderPage>
+    </>
+  );
 }
+
+const personalInfo = `11.5%`;
+const courtTypeTop = `32.8%`;
+const countyDistrictTop = `37.35%`;
+const page2Checkmarks = `18.15%`;
--- frontend/app/forms/order-on-motion-for-reduction-of-conviction/order-on-motion-for-reduction-of-conviction.web.component.js
+++ frontend/app/forms/order-on-motion-for-reduction-of-conviction/order-on-motion-for-reduction-of-conviction.web.component.js
@@ -1,5 +1,53 @@
 import React from "react";
+import FormThatPrints from "../inputs/form-that-prints.component";
+import TextInput from "../inputs/text-input.component";
+import Section from "../inputs/section.component.js";
+import Radio from "../inputs/radio.component.js";
+import GroupSelect from "../inputs/group-select.component.js";
+import {
+  resolvedByOptions,
+  courtTypeOptions,
+  JusticeCourtList,
+  DistrictCourtList
+} from "../form-common-options/form-common-options";
 
-export default function OrderOnMotionForReductionOfConvinction_Web(props) {
-  return <div>Order on Motion for Reduction of Conviction Web</div>;
+export default function OrderOnMotionForReductionOfConvinction_Web({ data }) {
+  return (
+    <FormThatPrints>
+      <Section name="1. Personal Information">
+        <TextInput dataKey="person.firstName" label="First Name" />
+        <TextInput dataKey="person.middleName" label={__("middle name")} />
+        <TextInput dataKey="person.lastName" label="Last Name" />
+        <TextInput dataKey="person.addressStreet" label={__("street")} />
+        <TextInput dataKey="person.addressCity" label={__("city")} />
+        <TextInput dataKey="person.addressState" label={__("state")} />
+        <TextInput dataKey="person.addressZip" label={__("zip")} />
+        <TextInput dataKey="person.homePhone" label={__("home phone")} />
+        <TextInput dataKey="person.email" label={__("email address")} />
+      </Section>
+      <Section name="2. Case Information">
+        <Radio
+          dataKey="case.courtType"
+          label="Court Type"
+          options={courtTypeOptions}
+        />
+        {data.case.courtType === "District" && (
+          <GroupSelect
+            dataKey="case.courtAddress"
+            label="District Courts"
+            groupOptions={DistrictCourtList}
+          />
+        )}
+        {data.case.courtType === "Justice" && (
+          <GroupSelect
+            dataKey="case.courtAddress"
+            label="Justice Courts"
+            groupOptions={JusticeCourtList}
+          />
+        )}
+        <TextInput dataKey="case.caseNumber" label={__("case number")} />
+        <TextInput dataKey="case.judgeName" label={__("judge full name")} />
+      </Section>
+    </FormThatPrints>
+  );
 }
