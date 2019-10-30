4596934e20710294f40af44eab3dd9b14a3e4aa9
Guillermo Villalta <mexcelus@gmail.com>
2019-10-30 00:16:40 -0600

    web and pdf components for form

---
+++ frontend/app/forms/order-on-motion-for-reduction-of-conviction/order-on-motion-for-reduction-of-conviction.pdf.component.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function OrderOnMotionForReductionOfConvinction_Pdf(props) {
+  return <div>Order on Motion for Reduction of Conviction PDF</div>;
+}
---
+++ frontend/app/forms/order-on-motion-for-reduction-of-conviction/order-on-motion-for-reduction-of-conviction.web.component.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function OrderOnMotionForReductionOfConvinction_Web(props) {
+  return <div>Order on Motion for Reduction of Conviction Web</div>;
+}
