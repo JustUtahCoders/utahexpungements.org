f9f46af22d532f5d229efb1036ba96e3e3f9b200
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2019-10-31 16:56:35 -0600

    added tests

--- backend/apis/docket-pdf-test-files/file.js
+++ backend/apis/docket-pdf-test-files/file.js
@@ -1,17 +1,47 @@
-const file = `SALT LAKE CITY JUSTICE COURT SALT LAKE COUNTY, STATE OF UTAH
-SALT LAKE CITY vs. FIRST MIDDLE LAST CASE NUMBER 000000000 Traffic Citation ________________________________________________________________________________ CHARGES
-Charge 1 - 53-3-202(1)(A) - NO VALID LICENSE - EXPIRED Class C Misdemeanor Offense Date: Month 00, 0000
-Disposition: Month 00, 0000 Dismissed (w/o prej) Charge 2 - 41-6A-601 - SPEEDING 70 in a 65 Class C Misdemeanor Offense Date: Month 00, 0000
-Disposition: Month 00, 0000 Dismissed (w/o prej) PARTIES
-Defendant - FIRST MIDDLE LAST Plaintiff - SALT LAKE CITY Represented by: FIRST MIDDLE LAST
+const file = `
+
+                         SALT LAKE CITY JUSTICE COURT
+                       SALT LAKE COUNTY, STATE OF UTAH
+
+                     SALT LAKE CITY vs. FIRST MIDDLE LAST
+CASE NUMBER 000000000 Traffic Citation
+________________________________________________________________________________
+CHARGES
+         Charge 1 - 53-3-202(1)(A) - NO VALID LICENSE - EXPIRED Class C
+         Misdemeanor
+         Offense Date: Month 00, 0000
+             Disposition: Month 00, 0000 Dismissed (w/o prej)
+         Charge 2 - 41-6A-601 - SPEEDING 70 in a 65 Class C Misdemeanor
+         Offense Date: Month 00, 0000
+             Disposition: Month 00, 0000 Dismissed (w/o prej)
+PARTIES
+         Defendant - FIRST MIDDLE LAST
+         Plaintiff -  SALT LAKE CITY
+         Represented by: FIRST MIDDLE LAST
+
 DEFENDANT INFORMATION
-Defendant Name: FIRST MIDDLE LAST Date of Birth: Month 00, 0000 Law Enforcement Agency: ZZZ - SALT LAKE Officer Name: FIRST MIDDLE LAST Prosecuting Agency: SALT LAKE CITY Citation Number: Z000000000
+         Defendant Name: FIRST MIDDLE LAST
+         Date of Birth: Month 00, 0000
+         Law Enforcement Agency: UHP - SALT LAKE
+         Officer Name: FIRST MIDDLE LAST
+         Prosecuting Agency: SALT LAKE CITY
+         Citation Number: Z000000000
+
 ACCOUNT SUMMARY
+
 CASE NOTE
-eCitation REPORT 00/00/0000 Z0000 #00 by FIRST MIDDLE LAST PROCEEDINGS 00-00-00 Case filed 00-00-00 Note: 0000 W SR-201 EB is outside city limits/sent orig cit to
-WVC JC - ticket scanned into system/ms 00-00-00 Charge 1 Disposition is Dismissed 00-00-00 Charge 2 Disposition is Dismissed 00-00-00 Case Closed
-Disposition Judge is FIRST MIDDLE LAST
-Printed: 00/00/00 00:00:00 Page 1 (last)
+         eCitation REPORT 00/00/0000 Z0000 #00 by  FIRST MIDDLE LAST
+PROCEEDINGS
+08-02-12 Case filed
+08-09-12 Note: 1400 W SR-201 EB is outside city limits/sent orig cit to
+               WVC JC - ticket scanned into system/ms
+08-09-12 Charge 1  Disposition is Dismissed
+08-09-12 Charge 2  Disposition is Dismissed
+08-09-12 Case Closed
+         Disposition Judge is FIRST MIDDLE LAST
+
+
+Printed: 00/00/0000 00:00:00          Page 1 (last)
 Page 1 of 1`;
 
 module.exports = { file: file };
