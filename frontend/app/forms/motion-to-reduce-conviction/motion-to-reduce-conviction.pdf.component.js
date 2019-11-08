090818063b4a28ff63c9ac8912364477ed25efc7
Ben Schinn <ben.schinn@mx.com>
2019-11-07 23:42:20 -0700

    personal information section

--- frontend/app/forms/motion-to-reduce-conviction/motion-to-reduce-conviction.pdf.component.js
+++ frontend/app/forms/motion-to-reduce-conviction/motion-to-reduce-conviction.pdf.component.js
@@ -1,5 +1,52 @@
 import React from "react";
+import RenderPage from "../render-page.component.js";
+import PositionedString from "../pdf-rendering/positioned-string.component.js";
+import PositionedCheckmark from "../pdf-rendering/positioned-checkmark.component.js";
 
-export default function MotionToReduceConviction_Pdf() {
-  return <div>Pdf component</div>;
+export default function MotionToReduceConviction_Pdf({ renderData, data }) {
+  return (
+    <>
+      <RenderPage url="/static/forms/motion-to-reduce-conviction/01_Motion_to_Reduce_Conviction-1.png">
+        <PositionedString debugKey="person.fullName" left="10.5%" top="14.1%">
+          <div>
+            {renderData("person.firstName")} {renderData("person.middleName")}{" "}
+            {renderData("person.lastName")}
+          </div>
+        </PositionedString>
+        <PositionedString
+          dataKey="person.addressStreet"
+          left="11.65%"
+          top="17.49%"
+        />
+        <PositionedString
+          debugKey="petitionerAddress"
+          left="11.65%"
+          top="21.09%"
+        >
+          {data.person.addressCity && `${renderData("person.addressCity")}, `}
+          {renderData("person.addressState")}
+          {`\u0020`}
+          {renderData("person.addressZip")}
+        </PositionedString>
+        <PositionedString
+          dataKey="person.homePhone"
+          left="11.65%"
+          top="24.5%"
+        />
+        <PositionedString dataKey="person.email" left="11.65%" top="28.1%" />
+        <PositionedCheckmark
+          dataKey="person.petitionerRepresentative"
+          left="20.82%"
+          top="62.2%"
+          shouldShow={data.person.petitionerRepresentative === "petitioner"}
+        />
+        <PositionedCheckmark
+          dataKey="person.petitionerRepresentative"
+          left="20.82%"
+          top="33.1%"
+          shouldShow={data.person.petitionerRepresentative === "attorney"}
+        />
+      </RenderPage>
+    </>
+  );
 }
--- frontend/app/forms/motion-to-reduce-conviction/motion-to-reduce-conviction.web.component.js
+++ frontend/app/forms/motion-to-reduce-conviction/motion-to-reduce-conviction.web.component.js
@@ -1,5 +1,36 @@
 import React from "react";
+import FormThatPrints from "../inputs/form-that-prints.component";
+import Section from "../inputs/section.component.js";
+import TextInput from "../inputs/text-input.component";
+import Select from "../inputs/select.component.js";
+import {
+  petitionerRepresentativeOptions,
+  courtTypeOptions,
+  JusticeCourtList,
+  DistrictCourtList
+} from "../form-common-options/form-common-options";
 
-export default function MotionToReduceConviction_Web() {
-  return <div>Web component</div>;
+export default function MotionToReduceConviction_Web({ data }) {
+  return (
+    <FormThatPrints>
+      <Section name="1. Personal Information">
+        <TextInput dataKey="person.firstName" label="First Name" />
+        <TextInput dataKey="person.middleName" label={__("middle name")} />
+        <TextInput dataKey="person.lastName" label="Last Name" />
+        <TextInput dataKey="person.addressStreet" label={__("street")} />
+        <TextInput dataKey="person.addressCity" label={__("city")} />
+        <TextInput dataKey="person.addressState" label={__("state")} />
+        <TextInput dataKey="person.addressZip" label={__("zip")} />
+        <TextInput dataKey="person.homePhone" label={__("home phone")} />
+        <TextInput dataKey="person.email" label={__("email address")} />
+      </Section>
+      <Section name="2. Case Information">
+        <Select
+          dataKey="person.petitionerRepresentative"
+          label={__("are you filling this out for yourself")}
+          options={petitionerRepresentativeOptions}
+        />
+      </Section>
+    </FormThatPrints>
+  );
 }
