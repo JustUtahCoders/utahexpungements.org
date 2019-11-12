f9971dbef54963e6e8f2ad61100e037d1240e77c
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2019-11-12 15:51:28 -0700

    trying to get the boy to retry

--- backend/apis/parse-docket-pdf.api.js
+++ backend/apis/parse-docket-pdf.api.js
@@ -7,6 +7,7 @@ app.post("/api/docket-pdfs", (req, res) => {
   let busboy;
   try {
     busboy = new Busboy({ headers: req.headers });
+    busboy.highWaterMark = 2 * 1024 * 1024; // Set 2MiB buffer
   } catch (err) {
     console.error(err);
     res.status(400).send({ error: err.message });
@@ -59,3 +60,15 @@ app.post("/api/docket-pdfs", (req, res) => {
 
   return req.pipe(busboy);
 });
+
+function retryBusboy(data, remainingAttempts) {
+  //console.log('retrying', remainingAttempts)
+  const promise = pdf(data);
+  return promise.catch(err => {
+    if (remainingAttempts > 0) {
+      return retryExtractPdfText(data, remainingAttempts - 1);
+    } else {
+      throw err;
+    }
+  });
+}
