581e4b57ce0e5aec951e396a66a5e45a6d517d02
lukeschunk <lukeschunk@gmail.com>
2020-01-14 19:45:19 -0700

    structures directories so each flag is its own file

--- backend/apis/generate-flag-json.utils.js
+++ backend/apis/generate-flag-json.utils.js
@@ -1,18 +1,10 @@
+const {
+  isDrugPosessionOffense
+} = require("./flag-utils/is-drug-conviction-flag-util");
+
 exports.generateFlagJson = function generateFlagJson(parsedObj) {
   console.log("parsedObj", parsedObj);
   return {
-    isDrugPosessionOffense: isDrugPosessionOffense(parsedObj),
-    isSentToDebtCollection: isSentToDebtCollection(parsedObj)
+    isDrugPosessionOffense: isDrugPosessionOffense(parsedObj)
   };
 };
-
-function isDrugPosessionOffense(parsedObj) {
-  console.log("this is parsedObj.offenseName", parsedObj.off);
-  //return true;
-
-  return parsedObj.charges[0].offenseName.toLowerCase().includes("drug");
-}
-
-function isSentToDebtCollection(parsedObj) {
-  return parsedObj.accountSummary[0].collection;
-}
