7dfd702e787b7635a6d7605b3772bf393fdd8bd2
Ben Schinn <ben.schinn@mx.com>
2020-01-21 21:04:56 -0700

    initial cut of debt collection flag

--- backend/apis/generate-flag-json.utils.js
+++ backend/apis/generate-flag-json.utils.js
@@ -1,9 +1,11 @@
 const {
   isDrugPosessionOffense
 } = require("./flag-utils/is-drug-conviction-flag-util");
+const { isDebtCollection } = require("./flag-utils/is-debt-collection-util");
 
 exports.generateFlagJson = function generateFlagJson(parsedObj) {
   return {
-    isDrugPosessionOffense: isDrugPosessionOffense(parsedObj)
+    isDrugPosessionOffense: isDrugPosessionOffense(parsedObj),
+    isDebtCollection: isDebtCollection(parsedObj)
   };
 };
