98bf0bf7a9e190dae465cece49858e44b89f6149
joevus <joehoskisson@gmail.com>
2020-02-04 11:43:30 -0700

    correct to check for at least one class b misdemeanor.

--- backend/apis/flag-utils/is-class-b-misdemeanor.js
+++ backend/apis/flag-utils/is-class-b-misdemeanor.js
@@ -1,15 +1,12 @@
 exports.isClassBMisdemeanor = function(parsedObj) {
-  // Determine if the individual has at least 4 convictions where 3 are Misdemeanor B's
-  // must have at least 4 convictions
-  if (parsedObj.charges.length < 4) {
-    return false;
-  }
-  let numOfClassBMisdemeanors;
-  // 3 or more of the convictions must be Class B Misdemeanors
-  for (let i = 0; i++; i < parsedObj.charges.length) {
-    if (parsedObj.charges[i].includes("B Misdemeanor")) {
-      numOfClassBMisdemeanors++;
+  // check if at least one charge is a Class B Misdemeanor
+  let containsClassBMisdemeanor = false;
+
+  parsedObj.charges.forEach(x => {
+    if (x.severity.includes("B Misdemeanor")) {
+      containsClassBMisdemeanor = true;
     }
-  }
-  return numOfClassBMisdemeanors >= 3;
+  });
+
+  return containsClassBMisdemeanor;
 };
