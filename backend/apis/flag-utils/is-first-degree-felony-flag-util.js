c1adf01b7fa8a690460ceab2fdd8b63478fb59eb
oliviaclyde <olivialclyde@gmail.com>
2020-02-11 19:40:20 -0700

    more work on logic

--- backend/apis/flag-utils/is-first-degree-felony-flag-util.js
+++ backend/apis/flag-utils/is-first-degree-felony-flag-util.js
@@ -1,7 +1,21 @@
 exports.isFirstDegreeFelony = function isFirstDegreeFelony(parsedObj) {
+  // if (
+  //   parseObj.charges.map(charge => charge.severity.toLowerCase()).includes("1st degree felony" && "amended")
+  // ){
+  //   return true;
+  // } else if (
+  //   parseObj.charges.map(charge => charge.severity.toLowerCase()).includes("1st degree felony")
+  // ){
+  //   return severity;
+  // } else {
+  //   return false
+  // }
+
+  console.log(parsedObj);
+
   if (
     parsedObj.charges[0].severity.toLowerCase().includes("1st degree felony") &&
-    parsedObj.charges[0].severity.toLowerCase().includes("(amended)")
+    parsedObj.charges[0].severity.toLowerCase().includes("(402 amended)")
   ) {
     return true;
   } else {
