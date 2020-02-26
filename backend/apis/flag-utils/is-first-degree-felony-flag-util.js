d0aa6e9e66243a99cc75f8e929a59012fa04e651
oliviaclyde <olivialclyde@gmail.com>
2020-02-26 09:51:50 -0700

    adjusted logic to return an array instead of loop independently through charges

--- backend/apis/flag-utils/is-first-degree-felony-flag-util.js
+++ backend/apis/flag-utils/is-first-degree-felony-flag-util.js
@@ -1,19 +1,8 @@
 exports.isFirstDegreeFelony = function isFirstDegreeFelony(parsedObj) {
-  if (
-    parsedObj.charges
-      .map(charge => charge.severity.toLowerCase())
-      .includes("1st degree felony")
-  ) {
-    if (
-      parsedObj.charges
-        .map(charge => charge.severity.toLowerCase())
-        .includes("amended")
-    ) {
-      return false;
-    } else {
-      return true;
-    }
-  } else {
-    return false;
-  }
+  const felonies = parsedObj.charges.filter(
+    charge =>
+      charge.severity.toLowerCase().includes("1st degree felony") &&
+      !charge.severity.toLowerCase().includes("amended")
+  );
+  return !!felonies.length;
 };
