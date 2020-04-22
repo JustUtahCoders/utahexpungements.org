496a7ead000954d2736106ace91215c85f032c5e
Mengqi Wang <wmq516@gmail.com>
2020-04-22 12:49:39 -0600

    added web and pdf components to print on png

--- frontend/app/forms/justice-court-cover-sheet/justice-court-cover-sheet.web.component.js
+++ frontend/app/forms/justice-court-cover-sheet/justice-court-cover-sheet.web.component.js
@@ -1,5 +1,43 @@
 import React from "react";
+import FormThatPrints from "../inputs/form-that-prints.component";
+import Section from "../inputs/section.component";
+import TextInput from "../inputs/text-input.component.js";
+import { petitionerRepresentativeOptions } from "../form-common-options/form-common-options";
+import Radio from "../inputs/radio.component.js";
 
-export default function JusticeCourtCoverSheet_Web(props) {
-  return <div>Justice Court Cover Sheet WEB Component</div>;
+export default function JusticeCourtCoverSheet_Web({ data }) {
+  return (
+    <FormThatPrints>
+      <Section name="1. Personal information">
+        <TextInput dataKey="person.firstName" label={__("first name")} />
+        <TextInput dataKey="person.middleName" label={__("middle name")} />
+        <TextInput dataKey="person.lastName" label={__("last name")} />
+        <TextInput dataKey="person.addressStreet" label={__("street")} />
+        <TextInput dataKey="person.addressCity" label={__("city")} />
+        <TextInput dataKey="person.addressState" label={__("state")} />
+        <TextInput dataKey="person.addressZip" label={__("zip")} />
+
+        <TextInput dataKey="person.homePhone" label={__("phone")} />
+        <TextInput dataKey="person.email" label={__("email address")} />
+
+        <Radio
+          dataKey="coversheet.isAttorney"
+          label="Are you the petitioner?"
+          options={petitionerRepresentativeOptions}
+        />
+        {data.coversheet && data.coversheet.isAttorney === "attorney" && (
+          <>
+            <TextInput
+              dataKey="person.petitionerAttorneyName"
+              label="Attorney Name"
+            />
+            <TextInput
+              dataKey="person.petitionerBarNumber"
+              label="Attorney Bar Number"
+            />
+          </>
+        )}
+      </Section>
+    </FormThatPrints>
+  );
 }
--- frontend/i18n/en.js
+++ frontend/i18n/en.js
@@ -220,7 +220,8 @@ const translations = {
 
   // justice court cover sheet
   "justice court cover sheet": "Justice Court Cover Sheet",
-  "justice court cover sheet short descr": "NEEDS DESCRIPTION",
+  "justice court cover sheet short descr":
+    "The cover sheet for the paperwork you file if your case is in a Justice Court.",
 
   // motion to waive fees
   "motion to waive fees name": "Motion to Waive Fees",
